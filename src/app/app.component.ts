430e18fd2c5b4dcf2eb726c5569de797de6068d1
Salvador Villa <salvadorvilla0810@gmail.com>
2025-04-25 19:51:45 -0400

    feat: input added and method created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'hacker-case-maker';
+  public generatedText: string = '';
+  public text: string = '';
+
+  generateText() {
+    console.log(this.text);
+
+    this.generatedText = this.text.toUpperCase().replace(/[OATUVSE ]/g, match => {
+        const mapping: { [key: string]: string } = {
+            'O': '0', 'A': '4', 'T': '7', 'U': 'V', 'V': 'U', 'S': '5', 'E': '3', 'G': '6', '6': 'G', ' ': '_'
+        };
+        return mapping[match] || match;
+    });
+
+    console.log(this.generatedText);
+}
+
+
 }
